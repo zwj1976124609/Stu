4cec50e042944ce3de5194386d0d266f388ad892
zwj <467330162@qq.com>
2019-05-20 15:35:25 +0800

    测试例子

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,20 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+// describe('My First Test', () => {
+//   it('Visits the app root url', () => {
+//     cy.visit('/')
+//     cy.contains('h1', 'Welcome to Your Vue.js App')
+//   })
+// })
+describe('My First Test', function () {
+  it('Does not do much!', function () {
+      cy.visit("https://www.baidu.com")
+
+      cy.get("#kw", {timeout: 2000}).type("cypress test")
+     
+      cy.wait(100)
+     
+      cy.get("#su", { timeout: 2000 }).click()
+
   })
 })
